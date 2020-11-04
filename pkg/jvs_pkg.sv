5b55646022f10198d6798f9bd896f07335fb2229
yli <shady831213@126.com>
2020-11-04 02:26:18 -0800

    add jitter comped

--- pkg/jvs_pkg.sv
+++ pkg/jvs_pkg.sv
@@ -2,7 +2,12 @@
  `define __JVS_PKG_SV__
 `include "jvs_defines.sv"
 `include "jvs_interfaces.sv"
+`ifndef JVS_PKG_TIMEPRECISION
+ `define JVS_PKG_TIMEPRECISION 1ps
+`endif
 package jvs_pkg;
+   timeunit 1ns;
+   timeprecision `JVS_PKG_TIMEPRECISION;
    import uvm_pkg::*;
 `include "uvm_macros.svh"
 `include "jvs_files.sv"
--- src/clk_rst_group/jvs_clk_driver.sv
+++ src/clk_rst_group/jvs_clk_driver.sv
@@ -3,7 +3,7 @@
 virtual class jvs_clk_driver_base#(type CFG_CLASS= jvs_clk_cfg_base) extends uvm_driver#(jvs_clk_trans_base#(CFG_CLASS));
    CFG_CLASS cfg;
    `uvm_field_utils_begin(jvs_clk_driver_base#(CFG_CLASS))
-     `uvm_field_object(cfg,UVM_ALL_ON)
+      `uvm_field_object(cfg,UVM_ALL_ON)
    `uvm_field_utils_end
 
    function new(string name = "jvs_clk_driver_base", uvm_component parent);
@@ -13,7 +13,7 @@ virtual class jvs_clk_driver_base#(type CFG_CLASS= jvs_clk_cfg_base) extends uvm
    virtual function void build_phase(uvm_phase phase);
       super.build_phase(phase);
       if(!uvm_config_db#(CFG_CLASS)::get(this, "", "cfg", cfg)) begin
-	 `uvm_fatal(get_full_name(), "Can't get cfg!");
+         `uvm_fatal(get_full_name(), "Can't get cfg!");
       end
    endfunction
 endclass // jvs_clk_driver_base
@@ -22,7 +22,7 @@ class jvs_root_clk_driver extends jvs_clk_driver_base#(jvs_root_clk_cfg);
    jvs_root_clk_trans trans;
    virtual jvs_root_clk_if vif;
    `uvm_component_utils_begin(jvs_root_clk_driver)
-     `uvm_field_object(trans, UVM_ALL_ON)
+      `uvm_field_object(trans, UVM_ALL_ON)
    `uvm_component_utils_end
 
    function new(string name = "jvs_root_clk_driver", uvm_component parent);
@@ -32,7 +32,7 @@ class jvs_root_clk_driver extends jvs_clk_driver_base#(jvs_root_clk_cfg);
    virtual function void build_phase(uvm_phase phase);
       super.build_phase(phase);
       if(!uvm_config_db#(virtual jvs_root_clk_if)::get(this, "", "jvs_root_clk_if", vif)) begin
-	 `uvm_fatal(get_full_name(), "Can't get root clk interface!");
+         `uvm_fatal(get_full_name(), "Can't get root clk interface!");
       end
       trans = jvs_root_clk_trans::type_id::create("jvs_root_clk");
       trans.cfg = cfg;
@@ -40,16 +40,22 @@ class jvs_root_clk_driver extends jvs_clk_driver_base#(jvs_root_clk_cfg);
 
    virtual task run_phase(uvm_phase phase);
       realtime delay;
+      bit [7:0] last_jitter = 0;
+      bit       last_jitter_flag = 0;
+      
       super.run_phase(phase);
       vif.clock = 0;
       delay = cfg.get_init_delay();
       #(delay);
       forever begin
-	 realtime half_period;
-	 vif.clock = ~vif.clock;
-	 trans.randomize();
-	 half_period = trans.get_half_period();
-	 #(half_period);
+         realtime half_period;
+         vif.clock = ~vif.clock;
+         trans.randomize();
+         trans.compensate(~last_jitter_flag, last_jitter);
+         half_period = trans.get_half_period();
+         last_jitter = trans.jitter;
+         last_jitter_flag = trans.jitter_flag;
+         #(half_period);
       end
    endtask
 endclass // jvs_root_clk_driver
@@ -59,7 +65,7 @@ endclass // jvs_root_clk_driver
 class jvs_gen_clk_driver extends jvs_clk_driver_base#(jvs_gen_clk_cfg);
    uvm_analysis_imp_clk_rst_driver#(jvs_clk_rst_trans, jvs_gen_clk_driver) rst_ana_imp;
    uvm_tlm_fifo#(jvs_clk_rst_trans) rst_fifo;
-   virtual 	  jvs_gen_clk_if vif;
+   virtual        jvs_gen_clk_if vif;
    `uvm_component_utils_begin(jvs_gen_clk_driver)
    `uvm_component_utils_end
 
@@ -71,37 +77,38 @@ class jvs_gen_clk_driver extends jvs_clk_driver_base#(jvs_gen_clk_cfg);
 
    virtual function void build_phase(uvm_phase phase);
       super.build_phase(phase);
+      cfg.check_div_ratio();
       if(!uvm_config_db#(virtual jvs_gen_clk_if)::get(this, "", "jvs_gen_clk_if", vif)) begin
-	 `uvm_fatal(get_full_name(), "Can't get gen clk rst interface!");
+         `uvm_fatal(get_full_name(), "Can't get gen clk rst interface!");
       end
    endfunction // build_phase
 
    local task drive_clk();
       bit [31:0] cycle_cnt;
-      int 	 i;
+      int        i;
       forever begin
-	 @vif.clk_driver;
-	 if(cycle_cnt == cfg.div_ratio - 1) begin
-	    vif.clk_driver.clock <= ~vif.clk_driver.clock;
-	    cycle_cnt = 0;
-	 end
-	 else begin
-	    cycle_cnt++;
-	 end
+         @vif.clk_driver;
+         if(cycle_cnt == cfg.div_ratio - 1) begin
+            vif.clk_driver.clock <= ~vif.clk_driver.clock;
+            cycle_cnt = 0;
+         end
+         else begin
+            cycle_cnt++;
+         end
       end
    endtask // drive_clk
 
    local task reset();
       if (cfg.sync_rst) begin
-	 vif.ctrl_driver.reset_n <= 0;
+         vif.ctrl_driver.reset_n <= 0;
       end
       else begin
-	 realtime delay = cfg.root_clk.get_period() * real'($urandom_range(1,255))/255;
-	 #(delay);
-	 vif.reset_n = 0;
+         realtime delay = cfg.root_clk.get_period() * real'($urandom_range(1,255))/255;
+         #(delay);
+         vif.reset_n = 0;
       end
       repeat(cfg.rst_cycle) begin
-	 @vif.ctrl_driver;
+         @vif.ctrl_driver;
       end
       vif.ctrl_driver.reset_n <= 1;
       @vif.ctrl_driver;
@@ -110,13 +117,13 @@ class jvs_gen_clk_driver extends jvs_clk_driver_base#(jvs_gen_clk_cfg);
    local task reset_begin();
       vif.rst_process = 1;
       repeat(3) begin
-	 @vif.clk_driver;
+         @vif.clk_driver;
       end
    endtask // reset_begin
 
    local task reset_end();
       repeat(2) begin
-	 @vif.ctrl_driver;
+         @vif.ctrl_driver;
       end
       vif.ctrl_driver.rst_process <= 0;
       @vif.ctrl_driver;
@@ -148,26 +155,26 @@ class jvs_gen_clk_driver extends jvs_clk_driver_base#(jvs_gen_clk_cfg);
       vif.clock = 0;
       vif.reset_n = 1;
       vif.rst_process= 0;
-      repeat(cfg.div_ratio-2) begin
-	 @vif.clk_driver;
+      repeat(cfg.div_ratio) begin
+         @vif.clk_driver;
       end
       fork
-	 drive_clk();
+         drive_clk();
       join_none
       forever begin
-	 reset_process();
+         reset_process();
       end
    endtask // run_phase
 
    virtual function void write_clk_rst_driver (jvs_clk_rst_trans tr);
       if (uvm_is_match(tr.pattern, {cfg.parent.get_name(), ".", cfg.get_name()})) begin
-	 if (!rst_fifo.try_put(tr)) begin
-	    `uvm_fatal(this.get_name(), "hw reset occurs while previous hw reset in processing!");
-	 end
+         if (!rst_fifo.try_put(tr)) begin
+            `uvm_fatal(this.get_name(), "hw reset occurs while previous hw reset in processing!");
+         end
       end
       else begin
-	 uvm_event end_e =  tr.end_event_pool.get(this.get_name());
-	 end_e.trigger();
+         uvm_event end_e =  tr.end_event_pool.get(this.get_name());
+         end_e.trigger();
       end
    endfunction
 endclass  
